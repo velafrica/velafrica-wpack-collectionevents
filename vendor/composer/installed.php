bcf8ba4df544f662df93a57f945252834b6f4a26
platzh1rsch <chregi.glatthard@gmail.com>
2021-06-05 18:29:08 +0100

    add vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '23e723b5f8a52084565cfef48675aa22dc45348d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23e723b5f8a52084565cfef48675aa22dc45348d',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.2.1',
+      'version' => '3.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1d405e739d32947fd87514a14d8b8bb9b744eabc',
+    ),
+  ),
+);
